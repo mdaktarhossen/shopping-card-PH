34e9f50a3d5b2989c7a89afa502ef2edb6ef52ef
mdaktarhossene <mdaktarhossen20@gmail.com>
2021-10-12 14:59:11 +0600

    mobile buy website PH

--- Practies.js
+++ Practies.js
@@ -14,6 +14,28 @@ mobileMinu.addEventListener('click', function(){
     const mobileNumber=parseFloat(mobileInput.value);
     const totalMobile=mobileNumber-1;
     mobileInput.value=totalMobile;
-    const totalPrice=totalMobile*1219;
-    document.getElementById('total-tk').innerText=totalPrice;
+    const totalMobilePrice=totalMobile*1219;
+    document.getElementById('total-tk').innerText=totalMobilePrice;
 })
+
+// ----------------------------second section
+const coverAdd=document.getElementById('addCover')
+coverAdd.addEventListener('click', function(){
+    const coverInput=document.getElementById('cover-number');
+    const coverNumber=parseFloat(coverInput.value);
+    const totalCover=coverNumber+1;
+    coverInput.value=totalCover;
+    const totalCoverPrice=totalCover*59;
+    document.getElementById('cover-tk').innerText=totalCoverPrice;
+})
+
+const minusCover=document.getElementById('minus-cover')
+minusCover.addEventListener('click', function(){
+    const coverInput=document.getElementById('cover-number');
+    const coverNumber=parseFloat(coverInput.value);
+    const totalCover=coverNumber-1;
+    coverInput.value=totalCover;
+    const totalCoverPrice=totalCover*59;
+    document.getElementById('cover-tk').innerText=totalCoverPrice;
+})
+
