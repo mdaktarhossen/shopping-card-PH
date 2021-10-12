d8d94560908370ce753af7199f2433ef07511cc6
mdaktarhossene <mdaktarhossen20@gmail.com>
2021-10-12 09:48:01 +0600

    this is shopping card PH

---
+++ Practies.js
@@ -0,0 +1,8 @@
+const addMobile=document.getElementById('addMobile')
+addMobile.addEventListener('click', function(){
+    const mobileNumer=document.getElementById('mobileNumer')
+    const total= parseFloat(mobileNumer);
+    console.log(total);
+
+    
+})
