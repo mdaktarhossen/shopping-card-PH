d95a69b09c0ab46f538e258c3713635703ab727f
mdaktarhossene <mdaktarhossen20@gmail.com>
2021-10-12 11:14:02 +0600

    mobile shopping

--- Practies.js
+++ Practies.js
@@ -1,8 +1,33 @@
 const addMobile=document.getElementById('addMobile')
 addMobile.addEventListener('click', function(){
-    const mobileNumer=document.getElementById('mobileNumer')
-    const total= parseFloat(mobileNumer);
-    console.log(total);
-
-    
+    const mobileNumber=document.getElementById('mobile-Number').value;
+    const total=parseFloat(mobileNumber);
+    const addMobile=total+1;
+    document.getElementById('mobile-Number').value=addMobile;
+    const totalPtices=addMobile*1219;
+    document.getElementById('cash-total').innerText='$'+totalPtices;
 })
+
+const minusMobile=document.getElementById('minus-mobile')
+minusMobile.addEventListener('click', function(){
+    // const mobileNumber=document.getElementById('mobile-Number').value;
+    // const total=parseFloat(mobileNumber);
+    // const addMobile=total-1;
+    // document.getElementById('mobile-Number').value=addMobile;
+    // const totalPtices=addMobile*1219;
+    // document.getElementById('cash-total').innerText=totalPtices;
+// })
+
+function mobilePricesUpdated(pricesMinusPlush){
+    const mobileNumber=document.getElementById('mobile-Number').value;
+    const total=parseFloat(mobileNumber);
+    if(pricesMinusPlush==true){
+        addMobile=total+1;
+    }
+    if(pricesMinusPlush==false && addMobile>0 ){
+        addMobile=total-1;      
+    }
+    document.getElementById('mobile-Number').value=addMobile;
+    const totalPtices=addMobile*1219;
+    document.getElementById('cash-total').innerText=totalPtices;
+}
